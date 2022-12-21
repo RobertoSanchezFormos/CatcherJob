e1a513b5d4f29cf0eddc58d626e2bc832bfca02e
RobertoSanchez <roberto.sanchez@formos.com>
2022-12-21 08:33:33 -0500

    Initial commit

---
+++ src/main/groovy/com/example/catcherjob/CatcherJobApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.catcherjob
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CatcherJobApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(CatcherJobApplication, args)
+    }
+
+}
---
+++ src/test/groovy/com/example/catcherjob/CatcherJobApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.example.catcherjob
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CatcherJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
